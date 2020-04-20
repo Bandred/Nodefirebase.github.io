6d57bdb1de5f61fc19e6aa879b0b7bc05b325036
Edson Barreto <edsonandreiu@hotmail.com>
2020-04-20 16:26:51 -0500

    mmm

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,33 +14,54 @@ const db = admin.database();
 
 router.get('/', (req, res) => {
 
-    db.ref('contacts').once('value', (snapshot) => {
+    db.ref('empresas').once('value', (snapshot) => {
         const data = snapshot.val();
-        res.render('index', { contacts: data });
+        res.render('index', { empresas: data });
     });
 
 });
 
-router.post('/new-contact', (req, res) => {
-
-    const newContact = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        phone: req.body.phone
+router.post('/new-empresa', (req, res) => {
+    
+    const newEmpresa = {
+        documento: req.body.documento,
+        documentodv: 5,
+        razonsocial: req.body.razonsocial,
+        diantipodocumentoidentidadid: 6,
+        diantipopersonaid: 1,
+        activo: true,
+        telefono: req.body.telefono,
+        entidadcertificadoraid: 0,
+        correoelectronicocontacto: req.body.correoelectronicocontacto,
+        nombrecontacto: req.body.nombrecontacto,
+        testid: '',
+        aplicafe: false,
+        enviadodian: false,
+        usuariovpid: null,
+        apikey: null,
+        fechacreaapikey: null,
+        observacion: req.body.observacion,
+        logo: '',
+        id: 0,
+        idstate: 0,
+        createdby: 1,
+        createdon: new Date(),
+        modifiedby: 1,
+        modifiedon: new Date()
     }
 
-    db.ref('contacts').push(newContact);
+    db.ref('empresas').push(newEmpresa);
     res.redirect('/');
 
 });
 
 
-router.get('/delete-contact/:id', (req, res) => {
+router.get('/delete-empresa/:id', (req, res) => {
 
-    db.ref('contacts/' + req.params.id).remove();
+    db.ref('empresas/' + req.params.id).remove();
     res.redirect('/');
-    
+
 });
 
 module.exports = router;
+
