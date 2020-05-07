2cb191238996e271a31c4c635293b1c3da74a4f1
Bandred <you@example.comedanbasu@gmail.com>
2020-05-07 18:50:11 -0500

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,27 +14,44 @@ const db = admin.database();
 
 router.get('/', (req, res) => {
 
+    res.render('index');
+
+});
+
+router.get('/empresas', (req, res) => {
+
     db.ref('empresas').once('value', (snapshot) => {
         const data = snapshot.val();
-        res.render('index', { empresas: data });
+        res.render('empresas', { empresas: data });
     });
 
 });
 
+router.get('/articulos', (req, res) => {
+
+    db.ref('articulos').once('value', (snapshot) => {
+        const data = snapshot.val();
+        res.render('articulos', { articulos: data });
+    });
+
+});
+
+
 router.post('/new-empresa', (req, res) => {
-    
+
     const newEmpresa = {
         documento: req.body.documento,
         documentodv: 5,
         razonsocial: req.body.razonsocial,
         diantipodocumentoidentidadid: 6,
         diantipopersonaid: 1,
+        diantiporegimenid: 1,
         activo: true,
         telefono: req.body.telefono,
         entidadcertificadoraid: 0,
         correoelectronicocontacto: req.body.correoelectronicocontacto,
         nombrecontacto: req.body.nombrecontacto,
-        testid: '',
+        testid: '111111',
         aplicafe: false,
         enviadodian: false,
         usuariovpid: null,
@@ -51,15 +68,62 @@ router.post('/new-empresa', (req, res) => {
     }
 
     db.ref('empresas').push(newEmpresa);
-    res.redirect('/');
+    res.redirect('/empresas');
 
 });
-
 
 router.get('/delete-empresa/:id', (req, res) => {
 
     db.ref('empresas/' + req.params.id).remove();
-    res.redirect('/');
+    res.redirect('/empresas');
+
+});
+
+router.post('/new-articulo', (req, res) => {
+
+    const newArticulo = {
+        codigo: req.body.codigo,
+        descripcion: req.body.descripcion,
+        activo: true,
+        empresaid: 6,
+        categoriaid: 2,
+        dianunidadmedidaid: 2,
+        imagen: null,
+        codigobarras: req.body.codigobarras,
+        valor: 200.0,
+        costo: 120.0,
+        color: null,
+        descuento: 1.0,
+        tipodescuento: null,
+        favorito: false,
+        precioeditable: true,
+        aplicaiva: true,
+        incluyeimpuesto: true,
+        neto: 120.0,
+        total: 200.0,
+        nombrearticulo: req.body.nombrearticulo,
+        valoriva: 1.0,
+        aplicainc: true,
+        valorinc: 1.0,
+        idowner: 1,
+        id: 0,
+        idstate: 0,
+        createdby: 1,
+        createdon: new Date(),
+        modifiedby: 1,
+        modifiedon: new Date()
+    }
+
+
+    db.ref('articulos').push(newArticulo);
+    res.redirect('/articulos');
+
+});
+
+router.get('/delete-articulo/:id', (req, res) => {
+
+    db.ref('articulos/' + req.params.id).remove();
+    res.redirect('/articulos');
 
 });
 
